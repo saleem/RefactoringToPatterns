d534015433d1215697855ff42735194ecd51922d
Saleem Siddiqui <saleem@saleem-siddiqui.com>
2016-10-06 08:21:38 -0500

    Wrote smarter first test for Singleton that fails

--- src/test/java/singleton/ElvisTest.java
+++ src/test/java/singleton/ElvisTest.java
@@ -3,6 +3,7 @@ package singleton;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
 
@@ -12,6 +13,8 @@ public class ElvisTest {
     public void iWasTheOne() {
         Elvis one = Elvis.instance();
         Elvis two = Elvis.instance();
+        assertNotNull(one);
+        assertNotNull(two);
         assertSame(one, two);
         assertEquals(one, two);
     }
